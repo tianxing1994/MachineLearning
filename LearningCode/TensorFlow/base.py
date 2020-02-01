299a7e593016ee82f9940b76e3ea4e50f5f143e4
unknown <miyukinakajima1994@gmail.com>
2020-02-01 16:38:03 +0800

    20200201163802

--- LearningCode/TensorFlow/base.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-test_labels = pd.DataFrame([1,0,0,1,1])
-predict_labels = pd.DataFrame([0,0,1,1,1])
-
-target = np.logical_xor(test_labels>0,predict_labels>0)
-print(target)
--- testCode/test_1.py
+++
@@ -1,7 +0,0 @@
-from sklearn.preprocessing import scale
-from sklearn.datasets import load_iris
-
-data = load_iris().data
-
-data_scaled = scale(data,axis=1)
-print(data_scaled)
